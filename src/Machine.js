0a0f8916070c2a5ac31f9c0c9bd16dee93ca0208
rionut0088 <74423983+rionut0088@users.noreply.github.com>
2020-11-23 17:56:43 -0500

    Update Machine.js

--- src/Machine.js
+++ src/Machine.js
@@ -1,6 +1,7 @@
 class Machine {
   constructor() {
     this.snacks = []
+    this.balance = 0;
   }
 
   seeSelections() {
@@ -8,10 +9,34 @@ class Machine {
   }
 
   stock(inventory) {
-    if(inventory == undefined) {
+    if (inventory == undefined) {
       throw Error("please do not troll. you cannot stock nothing.")
     }
     this.snacks = inventory
+  }
+  deposit(money) {
+    let validBills = [10, 20, 50, 100, 500]
+    if (validBills.indexOf(money) === -1) throw new Error("Please deposit valid bill")
+    this.balance += money;
+
+    return (`You have deposited $${this.balance}.`)
+  }
+  selectItem(item) {
+    let itemIndex = this.snacks.map((el) => el.name).indexOf(item)
+    if (itemIndex === -1) return ("The item you selected is unavailable")
+
+    if (this.snacks[itemIndex].price > this.balance) return `Your deposit is insufficient. Please add $${this.snacks[itemIndex].price - this.balance} for this item.`
+    else {
+      let balance = this.balance
+      this.balance = 0
+      return { item: item, change: balance - this.snacks[itemIndex].price };
+    }
+  }
+
+  cancel() {
+    let balance = this.balance
+    this.balance = 0
+    return { change: [balance] }
   }
 }
 
